e7a11ad416ae0d7343ec25f7b9ceae6c2762038d
moviestamilan <109806986+moviestamilan@users.noreply.github.com>
2022-07-24 11:13:31 +0530

    Update babel1.js

--- babel1.js
+++ babel1.js
@@ -31,7 +31,41 @@ const First = (props) =>{
 const App = () => {
   return(
      <span>
-       
+                <First src='img/thegrayman.jpg'
+         title='The Gray Man (2022) HD 720p Tamil Dubbed Movie Watch Online'  
+         href='http://vidmx.xyz/zembed-resbvsnuti6l.html'
+     
+          />
+          <First src='img/anythingspossible.jpg'
+         title='Anything’s Possible (2022) Tamil Dubbed Movie HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-pazn0xtt6pzw.html'
+     
+          />
+          <First src='img/jurassic.jpg'
+         title='Jurassic World Dominion (2022) Tamil Dubbed Movie HQ HDRip 720p Watch Online'  
+         href='http://vidmx.xyz/embed-sbu6plu7xyo9.html'
+     
+          />
+          <First src='img/shoorveer.jpg'
+         title='Shoorveer – S01 (2022) Tamil Dubbed Series HQ HDRip 720p Watch Online'  
+         href='http://vidmx.xyz/embed-wqn3up7677md.html'
+     
+          />
+          <First src='img/thorloveandthunder.jpg'
+         title='Thor: Love and Thunder (2022) Tamil Dubbed Movie HDCAMRip 720p Watch Online'  
+         href='http://vidmx.xyz/embed-0q9dtqv8dmun.html'
+     
+          />
+          <First src='img/persusion.jpg'
+         title='Persuasion (2022) Tamil Dubbed Movie HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-xcqyplkax11m.html'
+     
+          />
+          <First src='img/residentevil.jpg'
+         title='Resident Evil – S01 (2022) Tamil Dubbed Series HD 720p Watch Online'  
+         href='http://vidmx.xyz/embed-nhl852dwhhc0.html'
+     
+          />
 
     </span>
   )
